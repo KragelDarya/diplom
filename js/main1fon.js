4f95579caa9bbcd9e1c1995f120c02466b8269f4
KragelDarya <dasha12300227@gmail.com>
2025-12-05 22:25:00 +0300

    с

--- js/main1fon.js
+++ js/main1fon.js
@@ -24,3 +24,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+document.addEventListener("mousemove", e => {
+    const dot = document.createElement("div");
+    dot.className = "trail-dot";
+
+    dot.style.left = e.clientX + "px";
+    dot.style.top = e.clientY + "px";
+
+    document.body.appendChild(dot);
+
+    setTimeout(() => {
+        dot.remove();
+    }, 900);
+});
